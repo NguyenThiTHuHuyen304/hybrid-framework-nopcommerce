d42caec4065e16bfc8ba757efbc32ea0ea9565f0
Nguyen Huyen <huyen23813@gmail.com>
2023-04-14 14:48:13 +0700

    Multiple Browser

---
+++ testcase/com/nopcommerce/user/Level_04_Multiple_Browser.java
@@ -0,0 +1,59 @@
+package com.nopcommerce.user;
+
+import java.util.Random;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import commons.BaseTest;
+import pageObjects.HomePageObject;
+import pageObjects.LoginPageObject;
+
+public class Level_04_Multiple_Browser extends BaseTest {
+	private WebDriver driver;
+	private String InvalidEmail;
+	private HomePageObject homePage;
+	private LoginPageObject loginPage;
+
+	@Parameters("browser")
+	@BeforeClass
+	public void beforeClass(String browserName) {
+		driver = getBrowserDriver(browserName);
+
+		homePage = new HomePageObject(driver);
+		InvalidEmail = "123abc.com";
+
+	}
+
+	@Test
+	public void Login_01_Empty_Data() {
+		homePage.clickToLoginLink();
+		loginPage = new LoginPageObject(driver);
+		loginPage.clickToLoginButton();
+		Assert.assertEquals(loginPage.getErrorMessageAtEmailTextbox(), "Please enter your email");
+	}
+
+	@Test
+	public void Login_02_Invalid_Email() {
+		homePage.clickToLoginLink();
+		loginPage = new LoginPageObject(driver);
+		loginPage.inputToEmailTextbox(InvalidEmail);
+		loginPage.clickToLoginButton();
+		Assert.assertEquals(loginPage.getErrorMessageAtEmailTextbox(), "Wrong email");
+	}
+
+	public int fakeNumber() {
+		Random rand = new Random();
+		return rand.nextInt(9999);
+	}
+
+	@AfterClass
+	public void afterClass() {
+//		driver.quit();
+	}
+
+}
