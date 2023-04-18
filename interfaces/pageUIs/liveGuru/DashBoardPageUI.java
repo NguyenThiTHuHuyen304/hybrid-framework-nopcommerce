65265f8ce7676d56188ce2cf79bb7418193d43cb
Nguyen Huyen <huyen23813@gmail.com>
2023-04-18 18:06:28 +0700

    update

---
+++ interfaces/pageUIs/liveGuru/DashBoardPageUI.java
@@ -0,0 +1,9 @@
+package pageUIs.liveGuru;
+
+public class DashBoardPageUI {
+
+	public static final String REGISTER_SUCCESS_MESSAGE = "//li[@class= 'success-msg']//span";
+	public static final String ACCOUNT_BUTTON = "//header[@id = 'header']//span[text() = 'Account']";
+	public static final String LOGIN_BUTTON = "//a[text() = 'Log Out']";
+	public static final String LOGIN_SUCCESS_MESSAGE = "//div[@class = 'welcome-msg']//p[2]";
+}
